a9c59c4a90171f97c08031dca8bb28199b3bf531
CherryGamez <pavankvkumar@gmail.com>
2026-05-28 11:46:06 +0200

    some change to run config app

---
+++ postgres_db_creation/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE ipaas
+  WITH ENCODING 'UTF8'
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE   = 'en_US.UTF-8'
+       TEMPLATE   = template0;
+
+CREATE DATABASE valkeyry_config
+  WITH ENCODING 'UTF8'
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE   = 'en_US.UTF-8'
+       TEMPLATE   = template0;
---
+++ start_side_cars.bat
@@ -0,0 +1,7 @@
+docker run --name my-postgres-de `
+  -e POSTGRES_PASSWORD=valkeyry `
+  -e LANG=de_DE.utf8 `
+  -e POSTGRES_INITDB_ARGS="--locale=de_DE.utf8" `
+  -v "C:\Valkeyry\Git_Hub\valkeyry\postgres_schema\init.sql:/docker-entrypoint-initdb.d/init.sql" `
+  -p 5432:5432 `
+  -d postgres:16-alpine
